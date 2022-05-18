e0ae88c3429437af75f3391dd51260a011392e59
Georgi Inkov <georgi.inkov@scaleforce.io>
2022-05-18 14:15:40 +0300

    Inital commit

---
+++ 01.IntroAndBasicSyntax/01.StudentInformation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.StudentInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
